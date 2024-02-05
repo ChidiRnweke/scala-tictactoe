87fc0f1aff709bfb771d47a47185642be5abf4b4
ChidiRNweke <chidi125@gmail.com>
2024-02-05 14:43:20 +0100

    Version control generated game-logic

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import path from 'path';
 export default {
     input: 'js/target/scala-3.3.0/tictactoe-opt/main.js',
     output: {
-        file: 'dist/scala-tictactoe.js',
+        file: 'js/src/main/typescript/logic/game-logic.js',
         format: 'es',
         sourcemap: true
     },
